c118e5081baac81e99255e84fc912946968dd782
Martin jimenez <majiar16@gmail.com>
2024-06-27 14:35:25 -0500

    feat: add boostrap icons and add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { UserPageComponent } from './modules/dashboard/user-page/user-page.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: "users", component: UserPageComponent },
+  { path: "post", component: UserPageComponent },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: '**', redirectTo: 'users', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
