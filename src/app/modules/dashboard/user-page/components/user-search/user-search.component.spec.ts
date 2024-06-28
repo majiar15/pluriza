e9af50204dfb0be5c97120462772b8c0d8d476b5
Martin jimenez <majiar16@gmail.com>
2024-06-27 21:10:34 -0500

    feat: add user Serarch unit testing

--- src/app/modules/dashboard/user-page/components/user-search/user-search.component.spec.ts
+++ src/app/modules/dashboard/user-page/components/user-search/user-search.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserSearchComponent } from './user-search.component';
+import { By } from '@angular/platform-browser';
 
 describe('UserSearchComponent', () => {
   let component: UserSearchComponent;
@@ -8,10 +8,11 @@ describe('UserSearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserSearchComponent ]
-    })
-    .compileComponents();
+      declarations: [UserSearchComponent]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,17 @@ describe('UserSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit searchEvent on search', () => {
+    spyOn(component.searchEvent, 'emit');
+
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputElement.value = 'test search';
+    inputElement.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    expect(component.searchEvent.emit).toHaveBeenCalledWith('test search');
   });
 });
