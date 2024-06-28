71d58fb7140571453b55a7c9ce1580a86acc2c73
Martin jimenez <majiar16@gmail.com>
2024-06-27 21:10:51 -0500

    feat: add load more user unit testing

--- src/app/modules/dashboard/user-page/components/user-load-more/user-load-more.component.spec.ts
+++ src/app/modules/dashboard/user-page/components/user-load-more/user-load-more.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserLoadMoreComponent } from './user-load-more.component';
+import { By } from '@angular/platform-browser';
 
 describe('UserLoadMoreComponent', () => {
   let component: UserLoadMoreComponent;
@@ -8,10 +8,11 @@ describe('UserLoadMoreComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserLoadMoreComponent ]
-    })
-    .compileComponents();
+      declarations: [UserLoadMoreComponent]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserLoadMoreComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,22 @@ describe('UserLoadMoreComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit loadMoreEvent when onLoadMore is called', () => {
+    spyOn(component.loadMoreEvent, 'emit');
+
+    component.onLoadMore();
+
+    expect(component.loadMoreEvent.emit).toHaveBeenCalled();
+  });
+
+  it('should emit loadMoreEvent when the load more button is clicked', () => {
+    spyOn(component.loadMoreEvent, 'emit');
+
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+
+    expect(component.loadMoreEvent.emit).toHaveBeenCalled();
   });
 });
